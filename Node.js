d16a0bfb6e62f23348d93d331e7a909c2a9808f5
fnbd5002 <fnbd500@gmail.com>
2026-06-17 11:56:11 -0600

    Update Node.js

--- Node.js
+++ Node.js
@@ -1,17 +1,1 @@
-const express = require("express");
-const app = express();
 
-let lastEvent = null;
-
-app.use(express.json());
-
-app.post("/event", (req, res) => {
-    lastEvent = req.body;
-    res.json({ success: true });
-});
-
-app.get("/event", (req, res) => {
-    res.json(lastEvent || {});
-});
-
-app.listen(3000);
